26445c515a686ca1d0a0fdf305eee2c25b6b9401
Nill203 <trungluu1103@gmail.com>
2023-10-03 00:13:01 +0700

    js

--- src/main/webapp/assets/js/payment.js
+++ src/main/webapp/assets/js/payment.js
@@ -4,4 +4,20 @@
  * and open the template in the editor.
  */
 
+document.addEventListener("DOMContentLoaded", function () {
 
+    var radioInput = document.getElementById("exampleRadios2");
+    
+
+    var datePickerDiv = document.getElementById("datePickerDiv");
+
+
+    radioInput.addEventListener("change", function () {
+        // Kiểm tra nếu radio button được chọn, thì hiển thị thẻ div, ngược lại ẩn nó
+        if (radioInput.checked) {
+            datePickerDiv.style.display = "block";
+        } else {
+            datePickerDiv.style.display = "none";
+        }
+    });
+});
